867194bbfa07f1645cbebfba989029f0462caa30
João Roberto <jobond46@gmail.com>
2018-05-15 19:30:03 -0400

    "Receita"

---
+++ src/Model/ReceitaModel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bond
+ */
+public class ReceitaModel {
+    private int id;
+    private String nome;
+    private ArrayList<IngredienteModel> ingredientes;
+}
